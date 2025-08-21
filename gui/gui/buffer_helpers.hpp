2268b7a845f445ec0468ecfae96074ddc26e3d7c
Taanish R <tnreja@gmail.com>
2025-08-21 15:10:46 -0700

    refactored things into a much nicer pacakge

---
+++ gui/gui/buffer_helpers.hpp
@@ -0,0 +1,7 @@
+//
+//  buffer_helpers.hpp
+//  gui
+//
+//  Created by Taanish Reja on 8/21/25.
+//
+
---
+++ gui/gui/frame_info.hpp
@@ -0,0 +1,11 @@
+//
+//  FrameInfo.hpp
+//  gui
+//
+//  Created by Taanish Reja on 8/21/25.
+//
+
+struct FrameInfo {
+    float width;
+    float height;
+};
