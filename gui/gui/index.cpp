44ea4c526b92b203447e51f80a0e72f8c0caf30f
Taanish R <tnreja@gmail.com>
2026-01-21 23:32:41 -0800

    this commit is just me having fun after a long day wirh my library, shit is fun asf

--- gui/gui/index.cpp
+++ gui/gui/index.cpp
@@ -1,57 +1,46 @@
 #include "index.hpp"
-#include "context_manager.hpp"
-#include "events.hpp"
-#include "sizing.hpp"
-#include "new_arch.hpp"
 #include <print>
 #include <simd/vector_types.h>
 
-    // div(200, 200, simd_float4{0,1,0,1}).position(NewArch::Position::Absolute).left(100).top(200)
-    // .paddingLeft(20.0).addEventListener(EventType::MouseDown, [](auto& desc, const Event& event){ 
-    //         std::println("hello world!!");
-    //         desc.color = simd_float4{0,0.5,0,0.5};
-    //     })
-    // (
-    //     div(50, 50, simd_float4{1,1,1,1}),
-    //     text("hello \nworld", 64.0).color(simd_float4{0.0,0.0,1.0,1.0}).addEventListener(EventType::MouseDown, [](auto& desc, const Event& event){ 
-    //         std::println("hello world!!");
-    //         desc.text = "omg";
-    //     }).addEventListener(EventType::KeyDown, [](auto& desc, const Event& event){
-    //         char ch = event.get<EventType::KeyDown>().keyCode;
-    //         std::println("{}", ch);
-    //         desc.text = ch;
-
-    //     })
-    // );
-
 const std::string ArialBold = "/System/Library/Fonts/Supplemental/Arial Bold.ttf";
 
+static int count = 0;
+
 auto index() -> void {
+
     auto onClick = [](auto& desc, const Event& event){
-        std::println("hello world");
+        count += 1;
+
+        if (count % 2 == 0) {
+            desc.color = simd_float4{1.0,0.0,0.0,1.0};
+        }else {
+            desc.color = simd_float4{0.0,0.0,1.0,1.0};
+        }
+
+        std::println("hello world {}", count);
     };
 
     div(NewArch::Size::percent(1.0), NewArch::Size::percent(1.0), simd_float4{1.0,1.0,1.0,1.0})
-        // .cornerRadius(7.5)
-        // .paddingLeft(9.5)
-        // .paddingTop(4.5)
-        // .borderColor(simd_float4{0.77,0.71,1.0,1.0})
-        // .borderWidth(1.0)
+        .borderColor(simd_float4{0.77,0.71,1.0,1.0})
+        .borderWidth(1.0)
     (
-         div(NewArch::Size::px(60), NewArch::Size::px(30), simd_float4{0.498,0.0,1.0,1.0})
-            .position(NewArch::Absolute).left(20)
-            .top(30)
-            .cornerRadius(7.5)
-            .paddingLeft(9.5)
-            .paddingTop(4.5)
-            .borderColor(simd_float4{0.77,0.71,1.0,1.0})
-            .borderWidth(1.0)
-            .addEventListener(EventType::MouseDown, onClick)
-        (
-            text("Start")
-                .fontSize(18.0)
-                .font(ArialBold)
-        )
+         div(NewArch::Size::percent(0.2), NewArch::Size::percent(1.0), simd_float4{1.0,0.5,1.0,0.8})(
+            div(NewArch::Size::px(60), NewArch::Size::px(30), simd_float4{0.498,0.0,1.0,1.0})
+                .position(NewArch::Absolute).left(20)
+                .top(30)
+                .cornerRadius(7.5)
+                .paddingLeft(9.5)
+                .paddingTop(4.5)
+                .borderColor(simd_float4{0.77,0.71,1.0,1.0})
+                .borderWidth(1.0)
+                .addEventListener(EventType::MouseDown, onClick)
+            (
+                text("Start")
+                    .fontSize(18.0)
+                    .font(ArialBold)
+            ),
+            div(NewArch::Size::percent(0.5), NewArch::Size::percent(1.0), simd_float4{0.0,1.0,1.0,0.8})
+         )
     );
 }
 
