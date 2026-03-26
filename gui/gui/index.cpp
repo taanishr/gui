716118ba24027d49facb8857309cf359a9ecd966
Taanish R <tnreja@gmail.com>
2026-03-25 23:11:29 -0700

    refactored layoutphase (ty claude; did a good job with some steering)

--- gui/gui/index.cpp
+++ gui/gui/index.cpp
@@ -25,28 +25,40 @@ auto index() -> void {
 
 
 
-    div(NewArch::Size::percent(1.0), NewArch::Size::percent(1.0), simd_float4{1.0,1.0,1.0,1.0})
-        .position(NewArch::Position::Relative)
+    // div(NewArch::Size::percent(1.0), NewArch::Size::percent(1.0), simd_float4{1.0,1.0,1.0,1.0})
+    //     .position(NewArch::Position::Relative)
+    // (
+    //     div()
+    //         .width(NewArch::Size::percent(1.0))
+    //         // .height(NewArch::Size::px(100.0))
+    //         .color(simd_float4{0.9,0.9,0.9,1.0})
+    //         .display(NewArch::Display::Flex)
+    //         .flexGap(NewArch::Size::percent(0.01))
+    //         // .alignItems(NewArch::AlignItems::Center)
+    //         .flexDirection(NewArch::FlexDirection::Col)
+    //     (
+    //         div(NewArch::Size::px(90), NewArch::Size::px(80), simd_float4{1.0,0.3,0.3,1.0}),
+    //         div()
+    //         // .width(NewArch::Size::px(80))
+    //         .color(simd_float4{0.016,0.875,0.449,1.0})
+    //         (
+    //             text("hello world fsdfsdfsdfsdfsdfsdf").fontSize(NewArch::Size::pt(16.0)).color(simd_float4{0.0,0.0,0.0,1.0})
+    //         )
+    //         ,div(NewArch::Size::px(90), NewArch::Size::px(90), simd_float4{0.3,0.3,1.0,1.0})
+    //     )
+    // );
+
+div(NewArch::Size::percent(1.0), NewArch::Size::percent(1.0), simd_float4{1.0,1.0,1.0,1.0})
+(
+    div(NewArch::Size::px(200), NewArch::Size::px(300), simd_float4{0.9,0.9,0.9,1.0})
+        .display(NewArch::Display::Flex)
+        .flexDirection(NewArch::FlexDirection::Col)
     (
-        div()
-            .width(NewArch::Size::percent(1.0))
-            // .height(NewArch::Size::px(100.0))
-            .color(simd_float4{0.9,0.9,0.9,1.0})
-            .display(NewArch::Display::Flex)
-            .flexGap(NewArch::Size::px(2.8))
-            // .alignItems(NewArch::AlignItems::center)
-            .flexDirection(NewArch::FlexDirection::Col)
-        (
-            div(NewArch::Size::px(90), NewArch::Size::px(80), simd_float4{1.0,0.3,0.3,1.0}),
-            div()
-            // .width(NewArch::Size::px(80))
-            .color(simd_float4{0.016,0.875,0.449,1.0})
-            (
-                text("hello world fsdfsdfsdfsdfsdfsdf").fontSize(NewArch::Size::pt(16.0)).color(simd_float4{0.0,0.0,0.0,1.0})
-            )
-            ,div(NewArch::Size::px(90), NewArch::Size::px(90), simd_float4{0.3,0.3,1.0,1.0})
-        )
-    );
+        div(NewArch::Size::px(100), NewArch::Size::px(200), simd_float4{1.0,0.3,0.3,1.0})
+            .flexShrink(NewArch::Size::px(0)),
+        div(NewArch::Size::px(100), NewArch::Size::px(200), simd_float4{0.3,1.0,0.3,1.0})
+    )
+);
 
     //     div(NewArch::Size::percent(1.0), NewArch::Size::percent(1.0), simd_float4{1.0,1.0,1.0,1.0})
     //     .position(NewArch::Relative) // no containing block anymore; we shall resort to relativity here
